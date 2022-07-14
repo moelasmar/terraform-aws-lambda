78d72e2a39b93db6442740e7018e39d2ce194e00
Mohamed Elasmar <71043312+moelasmar@users.noreply.github.com>
2022-07-14 03:04:41 -0700

    Add files via upload

--- examples/simple/fixtures/languages_lib/languages_lib.py
+++ examples/simple/fixtures/languages_lib/languages_lib.py
@@ -1,1 +1,19 @@
+import pycountry
+from countryinfo import CountryInfo
 
+
+def get_country_language(country_name):
+    country = CountryInfo(country_name)
+    languages = []
+    for language_short_code in country.languages():
+        languages.append(get_language_by_short_code(language_short_code))
+    return languages
+
+
+def get_language_by_short_code(short_code):
+    language = pycountry.languages.get(alpha_2=short_code)
+    return {
+        "language_name": language.name,
+        "language_short_code": language.alpha_2,
+        "language_long_code" : language.alpha_3, 
+    } 
